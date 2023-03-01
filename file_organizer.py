45dcee551114150fc5f4737e2b4ce0928a23967c
Sneezedip <97994282+Sneezedip@users.noreply.github.com>
2023-03-01 15:13:17 -0100

    Update
    Added an option to change the directory.
    Added an option to quit the program
    Added a text to see which directory you're in
    Fixed some exception errors

--- file_organizer.py
+++ file_organizer.py
@@ -3,7 +3,11 @@ import shutil
 import time
 from colorama import Fore, Style
 import msvcrt as key
+import tkinter as tk
+from tkinter import filedialog
 
+root = tk.Tk()
+root.withdraw()
 textfiles = "X"
 pythonfiles = "X"
 pdffiles = "X"
@@ -19,9 +23,15 @@ shortcuts = "X"
 allinone = ""
 valid_inputs = {b'0', b'1', b'2', b'3', b'4',
                 b'5', b'6', b'7', b'8', b'9', b'a', b'b', b'c', b'd'}
+valid_inputs1 = {b'1', b'2', b'p', b'q'}
+python_folder = ""
+
+user_name = os.getlogin()
+desktopdir = f"C:\\Users\\{user_name}\\Desktop\\"
 
 
 def console():
+    os.system('cls')
     print(Fore.BLUE + Style.BRIGHT, """
      ▄▄▄▄▄▄▄ ▄▄▄ ▄▄▄     ▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄
     █       █   █   █   █       █  █       █   ▄  █ █       █       █  █  █ █   █       █       █   ▄  █
@@ -35,15 +45,29 @@ def console():
     print(Style.BRIGHT, f"""
     {Fore.WHITE}[1] - {Fore.GREEN}Run with default settings
     {Fore.WHITE}[2] - {Fore.YELLOW}Run with custom settings
-    """)
+    
+
+    {Fore.GREEN}[CURRENT DIRECTORY] - {Fore.RED}{desktopdir}
+    {Fore.GREEN}[P] - {Fore.RED}Change Directory
+    {Fore.GREEN}[Q] - {Fore.RED}Exit
+    """, Style.RESET_ALL)
     user_input = key.getch()
-    if user_input == b'1':
-        main()
-    if user_input == b'2':
-        custom()
+    if user_input in valid_inputs1:
+        if user_input == b'1':
+            main()
+        if user_input == b'2':
+            custom()
+        if user_input == b'p':
+            directorychange()
+        if user_input == b'q':
+            quit()
+    else:
+        os.system('cls')
+        console()
 
 
 def custom():
+    os.system('cls')
     global textfiles, pythonfiles, pdffiles, wordsfiles, powerfiles, excelfiles, compfiles, images, audiofiles, videofiles, programs, shortcuts, valid_inputs, allinone
 
     print(Fore.BLUE + Style.BRIGHT, """
@@ -59,7 +83,7 @@ def custom():
 
     print(Fore.BLUE + Style.BRIGHT, f"""
     {Fore.WHITE}[1] - {Fore.CYAN}Text files {Fore.RED}[{textfiles}]
-    {Fore.WHITE}[2] - {Fore.CYAN}Python files {Fore.RED}[{pythonfiles}]
+    {Fore.WHITE}[2] - {Fore.CYAN}Programming Files (ALL) {Fore.RED}[{pythonfiles}]
     {Fore.WHITE}[3] - {Fore.CYAN}PDF files {Fore.RED}[{pdffiles}]
     {Fore.WHITE}[4] - {Fore.CYAN}Words files {Fore.RED}[{wordsfiles}]
     {Fore.WHITE}[5] - {Fore.CYAN}Power files {Fore.RED}[{powerfiles}]
@@ -70,7 +94,7 @@ def custom():
     {Fore.WHITE}[a] - {Fore.CYAN}Video files {Fore.RED}[{videofiles}]
     {Fore.WHITE}[b] - {Fore.CYAN}Programs {Fore.RED}[{programs}]
     {Fore.WHITE}[c] - {Fore.CYAN}Shortcuts {Fore.RED}[{shortcuts}]
-    
+
     {Fore.WHITE}[d] - {Fore.YELLOW}ALL IN ONE FOLDER {Fore.RED}[{allinone}]
 
 
@@ -217,29 +241,14 @@ def custom():
 
 
 def main():
-    user_name = os.getlogin()
-    desktopdir = f"C:\\Users\\{user_name}\\Desktop\\"
+    os.system('cls')
+    global root, python_folder, py_folder, desktopdir
 
     print(Fore.BLUE + Style.BRIGHT, "Executing program with current settings...")
     for root, dirs, files in os.walk(desktopdir, topdown=True):
         if root == desktopdir:
             for name in files:
                 os.chdir(desktopdir)
-                if allinone == True or allinone == "X":
-                    allfolder = os.path.join(
-                        desktopdir, "All Folders")
-                    if not os.path.exists(allfolder):
-                        os.mkdir(allfolder)
-                    for item in os.listdir(desktopdir):
-                        item_path = os.path.join(desktopdir, item)
-
-                        if os.path.isdir(item_path):
-                            try:
-                                shutil.move(item_path, allfolder)
-                            except:
-                                pass
-                else:
-                    pass
                 if textfiles == True or textfiles == "X":
                     try:
                         if name.endswith('.txt'):
@@ -253,12 +262,25 @@ def main():
                     pass
                 if pythonfiles == True or pythonfiles == "X":
                     try:
-                        if name.endswith('.py'):
+                        if name.endswith('.py') or name.endswith('.pyw'):
                             py_folder = os.path.join(
                                 desktopdir, "Python Files")
                             if not os.path.exists(py_folder):
                                 os.mkdir(py_folder)
                             shutil.move(os.path.join(root, name), py_folder)
+                        if name.endswith('.cpp') or name.endswith('.c++') or name.endswith('.c') or name.endswith('.cc') or name.endswith('.cp') or name.endswith('.cxx') or name.endswith('.h') or name.endswith('.h++') or name.endswith('.hh') or name.endswith('.hpp') or name.endswith('.hxx') or name.endswith('.inc') or name.endswith('.inl') or name.endswith('.ipp') or name.endswith('.tcc') or name.endswith('.tpp'):
+                            c_folder = os.path.join(
+                                desktopdir, "C++ Files")
+                            if not os.path.exists(c_folder):
+                                os.mkdir(c_folder)
+                            shutil.move(os.path.join(root, name), c_folder)
+                        if name.endswith('.java'):
+                            java_folder = os.path.join(
+                                desktopdir, "Java Files")
+                            if not os.path.exists(java_folder):
+                                os.mkdir(java_folder)
+                            shutil.move(os.path.join(root, name), java_folder)
+
                     except:
                         pass
                 else:
@@ -289,7 +311,7 @@ def main():
                     pass
                 if powerfiles == True or powerfiles == "X":
                     try:
-                        if name.endswith('.pptx') or name.endswith('.ppt'):
+                        if name.endswith('.pptx') or name.endswith('.ppt') or name.endswith('.key') or name.endswith('.odp') or name.endswith('.pps'):
                             pptx_folder = os.path.join(
                                 desktopdir, "PowerPoint Files")
                             if not os.path.exists(pptx_folder):
@@ -313,7 +335,7 @@ def main():
                     pass
                 if compfiles == True or compfiles == "X":
                     try:
-                        if name.endswith('.zip') or name.endswith('.rar'):
+                        if name.endswith('.zip') or name.endswith('.rar') or name.endswith('.7z') or name.endswith('.arj') or name.endswith('.deb') or name.endswith('.pkg') or name.endswith('.rpm') or name.endswith('.z'):
                             zip_folder = os.path.join(
                                 desktopdir, "Compressed Files")
                             if not os.path.exists(zip_folder):
@@ -325,7 +347,7 @@ def main():
                     pass
                 if images == True or images == "X":
                     try:
-                        if name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith('.png') or name.endswith('.gif') or name.endswith('.jfif'):
+                        if name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith('.png') or name.endswith('.gif') or name.endswith('.jfif') or name.endswith('.ico') or name.endswith('.psd'):
                             img_folder = os.path.join(desktopdir, "Images")
                             if not os.path.exists(img_folder):
                                 os.mkdir(img_folder)
@@ -360,7 +382,7 @@ def main():
                     pass
                 if programs == True or programs == "X":
                     try:
-                        if name.endswith('.exe') or name.endswith('.msi'):
+                        if name.endswith('.exe') or name.endswith('.msi') or name.endswith('.bat'):
                             program_folder = os.path.join(
                                 desktopdir, "Programs")
                             if not os.path.exists(program_folder):
@@ -386,12 +408,45 @@ def main():
                     pass
         else:
             break
+        if allinone == True or allinone == "X":
+            allfolder = os.path.join(
+                desktopdir, "All Folders")
+            if not os.path.exists(allfolder):
+                os.mkdir(allfolder)
+            for item in os.listdir(desktopdir):
+                item_path = os.path.join(desktopdir, item)
+
+                if os.path.isdir(item_path):
+                    try:
+                        shutil.move(item_path, allfolder)
+                    except:
+                        pass
+        else:
+            pass
         print("Done.")
         time.sleep(0.5)
         print(Fore.BLUE + Style.BRIGHT,
               "Press any key to leave...", Style.RESET_ALL)
         key.getch()
         os.system('cls')
+
+
+def directorychange():
+    global desktopdir
+    os.system('cls')
+    print(Fore.BLUE + Style.BRIGHT,
+          "Press any key to change directory...", Style.RESET_ALL)
+    key.getch()
+    os.system('cls')
+    print(Fore.BLUE + Style.BRIGHT,
+          "Enter the new directory...", Style.RESET_ALL)
+    desktopdir = filedialog.askdirectory()
+    print(Fore.BLUE + Style.BRIGHT,
+          f"""{Fore.GREEN}New Directory : {desktopdir}\n{Fore.YELLOW}Press any key to continue...
+          """, Style.RESET_ALL)
+    key.getch()
+    os.system('cls')
+    console()
 
 
 console()
